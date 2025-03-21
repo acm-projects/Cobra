0423c27e490bec87295a40887be0ae5ba407049c
DavidMwaria <121251087+DavidMwaria@users.noreply.github.com>
2025-03-20 22:53:40 -0400

    deleted unneccessary files from pervious commit

--- background.js
+++
@@ -1,6 +0,0 @@
-/*chrome.webNavigation.onCompleted.addListener((details) => {
-    const url = details.url;
-    if (url.includes("leetcode.com/problems/")) {
-        chrome.action.openPopup(); // Opens the extension popup (May not work in all cases)
-    }
-}, { url: [{ hostSuffix: "leetcode.com" }] });*/
